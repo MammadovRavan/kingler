15b2c6787c64f2a2de69d298829b3755a25d12b1
MammadovRavan <rvanm15@gmail.com>
2023-07-08 23:55:23 +0400

    tamamlandi

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,129 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Form.css'
 const Form = () => {
-    return (
-        <div className='full-form'>
-            <div className="forms">
-                <input type="text" placeholder='Name' className='name' id='name' />
-                <small>Error message</small>
-            </div>
-            <div className="forms">
-                <input type="text" placeholder='E-mail' className='email' id='email' />
-                <small>Error message</small>
-            </div>
-            <div className="forms">
-                <input type="text" placeholder='Subject' className='subject' id='subject' />
-                <small>Error message</small>
-            </div>
-            <div className="forms">
-                <input type="text" placeholder='Message' className='message' id='message' />
-                <small>Error message</small>
-            </div>
-            <button>Submit</button>
-        </div>
-    )
-}
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  });
+  const [errors, setErrors] = useState({});
+  const [successMessage, setSuccessMessage] = useState('');
 
-export default Form
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validateForm(formData);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      setSuccessMessage('');
+    } else {
+      setErrors({});
+      setSuccessMessage('Message sent succesfully!');
+    }
+  };
+
+  const validateForm = (data) => {
+    const errors = {};
+
+    // Name validation (required field)
+    if (!data.name) {
+      errors.name = 'Name is required';
+    }
+
+    // Email validation (required and valid email format)
+    if (!data.email) {
+      errors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+      errors.email = 'Email is invalid';
+    }
+
+    // Subject validation (required field)
+    if (!data.subject) {
+      errors.subject = 'Subject is required';
+    }
+
+    // Message validation (required field)
+    if (!data.message) {
+      errors.message = 'Message is required';
+    }
+
+    return errors;
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className='forms'>
+        <input
+        placeholder='Name'
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          style={{ borderColor: errors.name ? 'red' : '' }}
+        />
+<div className="error"> 
+       {errors.name && <span>{errors.name}</span>}
+</div>
+      </div>
+
+      <div className='forms'>
+        <input
+        placeholder='E-mail'
+          type="text"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          style={{ borderColor: errors.email ? 'red' : '' }}
+        />
+<div className="error">
+{errors.email && <span>{errors.email}</span>}
+</div>
+      </div>
+
+      <div className='forms'>
+        <input
+        placeholder='Subject'
+          type="text"
+          name="subject"
+          value={formData.subject}
+          onChange={handleChange}
+          style={{ borderColor: errors.subject ? 'red' : '' }}
+        />
+      <div className="error">
+      {errors.subject && <span>{errors.subject}</span>}
+      </div>
+      </div>
+
+      <div className='form-text'>
+        <textarea
+        placeholder='Message'
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          style={{ borderColor: errors.message ? 'red' : '' }}
+        />
+      <div className="error">
+      {errors.message && <span>{errors.message}</span>}
+      </div>
+      </div>
+
+      <div className="form-button">
+      <button type="submit">SEND MESSAGE</button>
+      </div>
+
+<div className="success">
+{successMessage && <div style={{ color: 'green' }}>{successMessage}</div>}
+</div>
+    </form>
+  );
+};
+
+export default Form;
